af7aa18865dd1aa9e10ab5ccf64184b2c1618c74
mary-khachatryan <mary.khachatryan2@gmail.com>
2024-05-01 20:34:31 +0400

    next step:: adding totol mas

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from radio_buttons import Quiz_Form,MyForm
 from flask import Flask, render_template,url_for, request,redirect
+
+import openai
+import streamlit as st
+from openai import OpenAI
+from youtube_transcript_api import YouTubeTranscriptApi
+import os
+import json
+
 app = Flask(__name__)
 
 
@@ -13,10 +21,11 @@ app.config["SECRET_KEY"] = "11_509"
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    form = MyForm()
-    result = request.form
+    youtube_id = request.form.get('fname')
     if 'next_button' in request.form and request.method == 'POST':
         print("val")
+        youtube_id = request.form.get('fname')
+        print("id", youtube_id)
         return redirect('a')
         #return render_template('form_handler.html', title="youtube", handler='handler', result=result)
         #return render_template('form.html')\
