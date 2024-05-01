a33cfab84e43ccfb84262285c3376361d6c90bad
mary-khachatryan <mary.khachatryan2@gmail.com>
2024-05-01 20:03:07 +0400

    adding radio buttons with html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from radio_buttons import Quiz_Form
+from radio_buttons import Quiz_Form,MyForm
 from flask import Flask, render_template,url_for, request,redirect
 app = Flask(__name__)
 
@@ -9,11 +9,34 @@ app.config["SECRET_KEY"] = "11_509"
 # def myredirect():
 #     return redirect(url_for('quiz_form'))
 
+
+
 @app.route('/', methods=['GET', 'POST'])
+def home():
+    form = MyForm()
+    result = request.form
+    if 'next_button' in request.form and request.method == 'POST':
+        print("val")
+        return redirect('a')
+        #return render_template('form_handler.html', title="youtube", handler='handler', result=result)
+        #return render_template('form.html')\
+        #return render_template('form.html', title="kov", handler='handler')
+    return render_template('home.html', title="youtube", handler='handler')
+    # if form.validate_on_submit():
+    #     print("val")
+    #     return redirect(url_for('a'))
+    # return render_template('home.html', title="youtube", handler='handler', form=form, result=result)
+
+@app.route('/a', methods=['GET', 'POST'])
 def quiz_form():
     form = Quiz_Form()
-    if form.validate_on_submit():
+    if 'next_button' in request.form and request.method == 'POST':
         result = request.form
         return render_template('form_handler.html', title="youtube", handler='handler', result=result)
     return render_template('form.html', title='title', header='header2', form=form)
-    
+
+#  if request.method == 'POST':
+#         if 'next_button' in request.form:
+#             print("val")
+#             return redirect(url_for('a'))
+#     return render_template('home.html', title="youtube", handler='handler')
--- radio_buttons.py
+++ radio_buttons.py
@@ -1,11 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms import SubmitField,RadioField
 from wtforms.validators import InputRequired
+from wtforms.validators import DataRequired
 
 
 class Quiz_Form(FlaskForm):
+    
+    
     question = RadioField(u'Question',
                           choices=[('1', 'Choice 1'), ('2', 'Choice 2'), ('3', 'Choice 3')],
                           validators=[InputRequired(message="Choose right answer")]
                           )
-    submit = SubmitField("Submit")
+    
+    # submit = SubmitField("Submit")
+
+class MyForm(FlaskForm):
+  #  name = StringField('name', validators=[DataRequired()])  
+     submit = SubmitField("Next", validators=[DataRequired()])
